696b213d3ed388d8bf342c83f36e512563a13e51
manoelvgsilva <manoelvgsilva@gmail.com>
2024-08-24 10:38:13 -0300

    adicao de dependencia

---
+++ src/main/java/com/app/address/AddressApplication.java
@@ -0,0 +1,14 @@
+package com.app.address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * addressapplication.
+ */
+@SpringBootApplication
+public class AddressApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AddressApplication.class, args);
+  }
+}
