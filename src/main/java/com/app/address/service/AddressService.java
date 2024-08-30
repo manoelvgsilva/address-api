f624f3578eaf8a1dd8a5621c6d246b4aebce16d6
manoelvgsilva <manoelvgsilva@gmail.com>
2024-08-30 14:49:48 -0300

    route address with part errors

--- src/main/java/com/app/address/service/AddressService.java
+++ src/main/java/com/app/address/service/AddressService.java
@@ -3,6 +3,7 @@ package com.app.address.service;
 import com.app.address.entity.Address;
 import com.app.address.repository.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
