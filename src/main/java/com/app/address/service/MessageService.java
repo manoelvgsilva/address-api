5b4ad94aad6b616eaadc90a2fc8a7bc2def1c87d
manoelvgsilva <manoelvgsilva@gmail.com>
2024-12-03 09:11:10 -0300

    push messaging

--- src/main/java/com/app/address/service/MessageService.java
+++ src/main/java/com/app/address/service/MessageService.java
@@ -2,6 +2,7 @@ package com.app.address.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ public class MessageService {
   private final KafkaTemplate<String, String> kafkaTemplate;
   private static String staticTopcAddress;
 
+  @Autowired
   public MessageService(KafkaTemplate<String, String> kafkaTemplate, @Value(
       "${topic.address}") String topicAddress) {
     this.kafkaTemplate = kafkaTemplate;
